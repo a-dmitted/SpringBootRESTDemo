fd71b09114bfe575aaf031f6013a5b9120c93a4e
admitted <dmitriev.alexander@gmail.com>
2024-02-24 20:05:23 +0100

    add restart_server.sh

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-echo restart_server script has been triggered!
+
+# Define variables
+APP_NAME="SpringBootRESTDemo"
+JAR_NAME="SpringBootRESTDemo*.jar"
+APP_DIR="/opt/SpringBootRESTDemo"
+LOG_FILE="$APP_DIR/app.log"
+
+# Find the application's PID
+PID=$(ps -ef | grep $JAR_NAME | grep -v grep | awk '{ print $2 }')
+
+# If the application is running, kill it
+if [ ! -z "$PID" ]; then
+    echo "Stopping $APP_NAME..."
+    kill $PID
+    # Wait a bit to ensure the process has been killed
+    sleep 5
+fi
