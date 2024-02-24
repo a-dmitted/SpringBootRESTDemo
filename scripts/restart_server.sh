125eb06cd66a7938214cabc5caf21dd16d9deaf9
admitted <dmitriev.alexander@gmail.com>
2024-02-24 21:32:20 +0100

    source version from pom

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -7,7 +7,7 @@ APP_DIR="/opt/SpringBootRESTDemo"
 LOG_FILE="$APP_DIR/app.log"
 
 # Find the application's PID
-PID=$(ps -ef | grep $JAR_NAME | grep -v grep | awk '{ print $2 }')
+PID=$(ps -ef | egrep $APP_NAME".*\.jar" | grep -v grep | awk '{ print $2 }')
 
 # If the application is running, kill it
 if [ ! -z "$PID" ]; then
@@ -16,3 +16,9 @@ if [ ! -z "$PID" ]; then
     # Wait a bit to ensure the process has been killed
     sleep 5
 fi
+
+# Start the application
+echo "Starting $APP_NAME..."
+nohup java -jar $APP_DIR/$JAR_NAME > $LOG_FILE 2>&1 &
+
+echo "$APP_NAME started successfully."
--- src/main/java/com/example/demo/Greeting.java
+++ src/main/java/com/example/demo/Greeting.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
-public class Greeting {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
+public class Greeting {
+    private final static String APPLICATION_PROPERTIES = "application.properties";
+    private final static String VERSION_PROPERTY = "version";
+    private static final String version = readVersion();
     private final String content;
 
     public Greeting(String content) {
@@ -13,6 +19,28 @@ public class Greeting {
     }
 
     public String getVersion() {
-        return "3.24.1";
+        return version;
+    }
+
+    private static String readVersion() {
+        Properties prop = new Properties();
+        String version = "unknown";
+
+        try (InputStream input = Greeting.class.getClassLoader().getResourceAsStream(APPLICATION_PROPERTIES)) {
+            if (input == null) {
+                System.out.printf("Sorry, unable to find %d%n", APPLICATION_PROPERTIES);
+                return version;
+            }
+
+            // Load the properties file
+            prop.load(input);
+
+            // Get the property value
+            version = prop.getProperty(VERSION_PROPERTY);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return version;
     }
 }
